7525ada75dc5955f517608efab482b53d78dd60f
daiana98 <quispedaivioleta@gmail.com>
2024-11-02 14:18:53 -0300

    Se agrega el boton perfil en la pagina inicio

--- js/cerrar-session.js
+++ js/cerrar-session.js
@@ -1,11 +1,15 @@
 let botonSesion = document.querySelector(".header__principal-iniciar-sesion");
+//let botonPerfil = document.querySelector(".header__principal-perfil");
+let botonPerfil = document.getElementById('header__principal-perfil');
 
 console.log(botonSesion);
 
 if(sessionStorage.getItem("usuarioLogueado")!= null){
     botonSesion.innerHTML = "Cerrar sesión";
-    
+    botonPerfil.style.display='block';
+
     botonSesion.addEventListener("click",()=>{
         sessionStorage.clear();
+        botonPerfil.style.display='none';
     })
 } 
