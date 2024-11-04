b0cd126e32e375d702d272544f4d96d95926b1d4
alanMarcosA <alanaspi1997@gmail.com>
2024-11-04 16:29:27 -0300

    Agrega validacion para prevenir Undefined de los cursos agregados al carito

--- js/carrito.js
+++ js/carrito.js
@@ -18,24 +18,27 @@ function hacerDinamicoLosCursosDestacados() {
     let subtotal = 0; // Inicializa subtotal
 
     sectionMostrarCarrito.innerHTML = ""; 
-    carritoDB.cursos_a_comprar.forEach((item,index) => {
-        const nuevaTarjeta = document.createElement('div');
-        nuevaTarjeta.className = "producto";
-
-        nuevaTarjeta.innerHTML =
-            `<img src="../${item.curso.imagen_url}" alt="Imagen del curso ${item.curso.nombre}">
-            <div class="producto-info">
-                <p class="producto-nombre">${item.curso.nombre}</p>
-                <p class="producto-cantidad">Cantidad: ${item.cantidad}</p>
-                <p class="producto-precio">$${item.curso.precio.toFixed(2)}</p>
-                <button class="btn-eliminar" data-index="${index}">X</button>
-            </div>`;
-            console.log(nuevaTarjeta);
-
-            subtotal += item.curso.precio * item.cantidad;
-
-            sectionMostrarCarrito.appendChild(nuevaTarjeta);
-    });
+    if(carritoDB == []){
+        carritoDB.cursos_a_comprar.forEach((item,index) => {
+            const nuevaTarjeta = document.createElement('div');
+            nuevaTarjeta.className = "producto";
+    
+            nuevaTarjeta.innerHTML =
+                `<img src="../${item.curso.imagen_url}" alt="Imagen del curso ${item.curso.nombre}">
+                <div class="producto-info">
+                    <p class="producto-nombre">${item.curso.nombre}</p>
+                    <p class="producto-cantidad">Cantidad: ${item.cantidad}</p>
+                    <p class="producto-precio">$${item.curso.precio.toFixed(2)}</p>
+                    <button class="btn-eliminar" data-index="${index}">X</button>
+                </div>`;
+                console.log(nuevaTarjeta);
+    
+                subtotal += item.curso.precio * item.cantidad;
+    
+                sectionMostrarCarrito.appendChild(nuevaTarjeta);
+        });
+    }
+    
     // Mi resu de carrito
     const carritoResumen = document.createElement('div');
     carritoResumen.className = "carrito-resumen";
