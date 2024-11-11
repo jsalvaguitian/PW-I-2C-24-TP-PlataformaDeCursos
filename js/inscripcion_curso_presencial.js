670c81e7cd5172ffdbdb74b26f91db4ac77dbb8e
Leoft2 <leonelvalenzuela110@gmail.com>
2024-11-11 12:34:53 -0300

    InscripcionCursoPresencial JS v3
    Cambios html y js. Funcionalidad mostrar datos del curso y agregar al carrito el curso presencial

--- js/inscripcion_curso_presencial.js
+++ js/inscripcion_curso_presencial.js
@@ -1,13 +1,49 @@
 import { Inscripto } from "./ClaseInscriptos.js";
+import { buscarEntidadEnLocalStorage, buscarEntidadEnSessionStorage } from "./ayudante.js";
 
+
+// Inicializar variables y arrays
 const inscriptos = [];
 const inscripcion= [];
+let contadorP = 1;
+let importe_nuevo = 0;
+let importe_curso = 0;
+
+// CARRITO
+
+// Recuperar el carrito para guardar datos
+let carrito = JSON.parse(sessionStorage.getItem("carrito"));
+let botonCarrito = document.querySelector(".header__principal-carrito");
+let textoCarrito = botonCarrito.querySelector(".texto-carrito");
+
+// Verificar si carrito no existe y modifica contador carrito
+
+
+if (sessionStorage.getItem("carrito") == null) {
+    sessionStorage.setItem("carrito", JSON.stringify(new Carrito()));
+}
+textoCarrito.innerHTML = carrito.cantidad_total;
+
+// Buscar curso presencial segun el curso seleccionado
+let indiceCursoPresencial =JSON.parse(sessionStorage.getItem("indiceCursoAVer"));
+let cursoPresencialBuscado = buscarPorIdCursoDB(indiceCursoPresencial); 
+
+ponerDatosDelCursoSeleccionado(cursoPresencialBuscado);
+
+
+function buscarPorIdCursoDB(indice){
+    let cursosDB = buscarEntidadEnLocalStorage("curso");
+    for (let i = 0; i < cursosDB.length; i++) {
+        if (cursosDB[i].id == indice)
+            return cursosDB[i];
+    }
+    return null;
+}
+
 // Funcionalidad agregar campos de personas en el form
 const btn_agregar_persona = document.querySelector('.person-btn.add');
 btn_agregar_persona.addEventListener('click',agregar_persona);
 
-let contadorP = 1;
-let importe_nuevo = 25000
 
 function agregar_persona(event) {
 
@@ -37,7 +73,7 @@ function agregar_persona(event) {
     </div>
     <div class="input-group">
         <label for="importe${contadorP}">Importe:</label>
-        <input type="text" id="importe${contadorP}" name="importe" value="25000" disabled required>
+        <input type="text" id="importe${contadorP}" name="importe" value="${importe_curso}" disabled required>
     </div>
     <div class="delete-person">
         <button type="button" class="person-btn delete" name="btn-delete${contadorP}" onclick="eliminar_persona(${contadorP})" > − </button>
@@ -47,7 +83,7 @@ function agregar_persona(event) {
     form.appendChild(person_data);
 
     //recalcular importe total
-    importe_nuevo +=25000;
+    importe_nuevo +=importe_curso;
     
     let contenedor_importe = document.querySelector('.payment-info');
     let importe_total = contenedor_importe.querySelector('p');
@@ -79,7 +115,7 @@ function eliminar_persona(indice) {
         personaActual.parentNode.removeChild(personaActual);
         
         //recalcular importe total
-        importe_nuevo -=25000;
+        importe_nuevo -=importe_curso;
         let contenedor_importe = document.querySelector('.payment-info');
         let importe_total = contenedor_importe.querySelector('p');
         importe_total.textContent=`$${importe_nuevo}.-`;
@@ -87,6 +123,12 @@ function eliminar_persona(indice) {
     }
 
 }
+
+
+
+let contadorEnvio = 0; // evitar que ingrese dos veces a la funcion del boton agregar carrito y duplique los datos
+
+// funcion de evitar envio formulario y tomar los datos de los inscriptos para el carrito
 
 const form = document.getElementById('registrationForm');
 form.addEventListener('submit', evitarEnvio)
@@ -119,7 +161,7 @@ function evitarEnvio(evento) {
     // Agregar el array inscriptos y importe total al array inscripcion
     inscripcion.push(inscriptos);
     inscripcion.push(importe_nuevo);
-    //console.log(inscripcion);
+    
 
     //modal box
 
@@ -143,8 +185,17 @@ function evitarEnvio(evento) {
 
     const carritoBTN = document.querySelector('.add-cart-btn');
     carritoBTN.addEventListener('click', function() {
-        guardarInscripcionLocalStorage();
-        form.submit();
+        contadorEnvio++;
+        //solo hace una sola vez, en caso que llame 2 veces o mas
+        if(contadorEnvio==1) {
+            let cursoPresencial = agregarInscriptosAlCursoPresencial();
+            guardarCursoPresencialCarrito(cursoPresencial,carrito);
+            sessionStorage.setItem("carrito", JSON.stringify(carrito));
+            textoCarrito.innerHTML = carrito.cantidad_total;
+            form.submit();
+        }
+         
+
     });
 
 } 
@@ -152,7 +203,7 @@ function evitarEnvio(evento) {
 function mostrarResumenInscriptosModal() {
     const inscriptos_list = document.querySelector('.js-inscriptos-list');
     inscriptos_list.innerHTML = ""; 
-
+    
     inscriptos.forEach((inscripto, index) => {
         let inscripto_data = document.createElement("div");
         inscripto_data.className = 'inscripto-data';
@@ -169,14 +220,95 @@ function mostrarResumenInscriptosModal() {
 
 }
 
-function guardarInscripcionLocalStorage() {
-    localStorage.setItem('inscripcion', JSON.stringify(inscripcion));
+
+function agregarInscriptosAlCursoPresencial() {
+    
+    let inscriptosForm = inscripcion[0];
+    let inscriptosCurso = cursoPresencialBuscado.inscriptos;
+
+    // agrega los inscriptos al curso presencial
+    inscriptosForm.forEach((inscripto, index) => {
+        inscriptosCurso.push(inscripto);
+    });
+    //console.log(cursoPresencialBuscado);
+    return cursoPresencialBuscado;
 }
+
+
+function guardarCursoPresencialCarrito(cursoPresencial,carrito) {
+
+    if (carrito.cursos_a_comprar.length == 0) {
+        agregarUnaFilaACarrito(carrito, cursoPresencial);
+        calcularPrecioTotal(carrito, cursoPresencial);
+    } else {
+        if (existeCursoEnCarrito(carrito, cursoPresencial) == false) {
+            agregarUnaFilaACarrito(carrito, cursoPresencial);
+            calcularPrecioTotal(carrito, cursoPresencial);
+        }
+    } 
+
+    
+}
+
+// agregar info/entrada al carrito
+
+function agregarUnaFilaACarrito(carrito, cursoPresencial) {
+    carrito.cantidad_total += 1;
+    let filaCarrito = crearFilaTablaCarrito(1, cursoPresencial);
+    carrito.cursos_a_comprar.push(filaCarrito);
+}
+
+function crearFilaTablaCarrito(cantidad, cursoPresencial) {
+    let filaCarrito = {
+        cantidad: cantidad,
+        curso: cursoPresencial
+    }
+
+    return filaCarrito
+
+}
+
+
+function calcularPrecioTotal(carrito,cursoPresencial) {
+    carrito.precio_total +=cursoPresencial.precio
+}
+
+// ver si existe duplicado
+function existeCursoEnCarrito(carrito, cursoPresencial) {
+
+    for (let i = 0; i < carrito.cursos_a_comprar.length; i++) {
+        if (carrito.cursos_a_comprar[i].curso.id == cursoPresencial.id) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+function ponerDatosDelCursoSeleccionado(cursoPresencialBuscado) {
+    const contenedor_titulo_curso = document.querySelector('.registration-titulos');
+    const titulo_curso = contenedor_titulo_curso.querySelector("h1");
+    let nombreCurso = cursoPresencialBuscado.nombre;
+    titulo_curso.textContent = nombreCurso;
+
+    let importeCurso = cursoPresencialBuscado.precio;
+    importe_curso = importeCurso;
+    importe_nuevo = importeCurso;
+
+    const total_amount = document.getElementById("totalAmount");
+    total_amount.textContent = `$${importe_curso}.`;
+
+    const importe_primeraPersona = document.getElementById("importe1");
+    importe_primeraPersona.value = importe_curso;
+    
+}
+
+
+
 
 
 // fix import
 window.eliminar_persona = eliminar_persona;
-
 
 
 
